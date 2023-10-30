22f552a31a17909005d6113e6f94fbcf5afe5c7b
SAYED <mohammedcm046@gmail.com>
2023-10-30 11:58:36 +0530

    Commit Done-2

--- RockPush/src/Package/Rockers.java
+++ RockPush/src/Package/Rockers.java
@@ -5,6 +5,7 @@ public class Rockers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Rockers");
+		//if u want to read line of array from console or execution display then use the format
 	}
 
 }
